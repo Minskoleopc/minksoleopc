c7587626d5b28a3fffaf53bd14010f3d6b557421
unknown <Minskoleopc>
2023-10-06 20:23:20 -0700

    added

---
+++ script2M.js
@@ -0,0 +1,176 @@
+// // let and const 
+
+// let a = 10
+// console.log(a)
+// a = 899
+// console.log(a)
+
+
+// const h = 100
+// console.log(h)
+// //h = 909
+
+// // program 2
+
+// let a1 = 10 
+// let b2 = 5
+
+// console.log(a1+b2)
+// console.log(a1-b2)
+// console.log(a1*b2)
+// console.log(a1/b2)
+// console.log(a1%b2)
+
+// // program 3
+// let q = 8 
+// let r = 4
+
+// console.log(q+r)
+// console.log(q-r)
+// console.log(q*r)
+// console.log(q/r)
+// console.log(q%r)
+
+// // program 4
+// function Calculator(x,y){
+//     console.log(x+y)
+//     console.log(x-y)
+//     console.log(x*y)
+//     console.log(x/y)
+//     console.log(x%y)
+// }
+// Calculator(12,4)
+// Calculator(8,4)
+
+// //program 5
+
+
+// function addA(){
+//     console.log(9+9)
+// }
+// addA()
+// addA()
+// addA()
+// addA()
+
+
+// function addB(x,y){
+//     console.log(x+y)
+   
+// }
+// addB(12,4)
+// addB(12,6)
+// addB(12,2)
+
+
+// function addC(x,y){
+//     return x+y
+// }
+
+// let q1 = addC(22,55)
+// console.log(q1+q1)
+// console.log(q1-q1)
+// console.log(q1*0.10)
+
+
+// Introvert       Extrovert 
+
+// calm             loud
+// less social      more social
+// less outing      more outing 
+
+// Human 
+// Properties - weight , height , color , gender
+// Methods - walk() , talk()
+
+// Vehicle 
+// Properties - model , type , regNo , color 
+// Method - start() , stop()
+
+// Bank acc 
+// Propeties - accName , accNo , accType , balance , IFSCcode , branchName 
+// Methods - deposit() , withdrawl()
+
+
+let x = 10
+console.log(x)
+console.log(typeof x) 
+// 9 . -9 , -89.8 , 89.9 , 0
+
+let y = true
+console.log(y)
+console.log(typeof y)
+// true or false
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z)
+// " ", '',   ``
+//"A" , 'a', "1","123@123143##" , " "
+
+// comparison operator 
+// entity < entity -----> boolean ----> true or false
+
+console.log(typeof 10)
+console.log(typeof '10')
+
+// <, > , <= , >=
+// != , ==  // value 
+// !== , === // value and type 
+
+console.log(10 == '10')
+console.log(10 === '10')
+console.log(10 !== '10')
+console.log(10 === 10)
+console.log(10 !== 10)
+console.log(10 != 10)
+console.log(10 != '10')
+console.log(8 > 4)
+console.log(8 < 4)
+console.log(8 >= 4)
+console.log(8 <= 4)
+console.log(8 >= 8)
+console.log(8 <= 8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

34d33c552aed071612917c8c3a77127af3b97487
unknown <Minskoleopc>
2023-11-22 06:00:26 -0800

    added

---
+++ DOM7/script.js
@@ -0,0 +1,12 @@
+
+//     <h1>Chinmay</h1>
+
+let headOne = document.querySelector('h1')
+
+headOne.addEventListener('click',function(){
+                           // CHINMAY
+    headOne.textContent = headOne.textContent.toUpperCase()
+    headOne.style.color = "green"
+
+})
+

0070868035bc3c4f1075682d8f12262060d076a2
unknown <Minskoleopc>
2023-11-24 06:04:22 -0800

    added

---
+++ DOM11/script.js
@@ -0,0 +1,8 @@
+
+let allELement = document.querySelector('body')
+
+allELement.addEventListener('click',function(event){
+    //console.log(event.target)
+    //console.log(event.target.tagName)
+    console.log(event.target.className)
+})
